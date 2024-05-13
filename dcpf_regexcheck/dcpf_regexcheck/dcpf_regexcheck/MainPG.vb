d2982380b0fc806dbf43b6ce8e46d2d98eb9a571
saramake <sadayoshi.aramaki@outlook.jp>
2024-05-13 19:23:05 +0900

    なんか修正した

---
+++ dcpf_regexcheck/dcpf_regexcheck/dcpf_regexcheck/MainPG.vb
@@ -0,0 +1,59 @@
+﻿Imports dcpf_regexcheck.regexcheck
+Imports Newtonsoft.Json
+
+Module MainPG
+
+    Sub Main()
+
+    End Sub
+
+    ' JSONファイルの出力（配列データ）
+    Public Sub WriteJsonArrayToFile()
+
+        Dim Targets As Target() = {New Target With {
+                                                    .Label = "BVR同期警告",
+                                                    .Folder = "%WORKFOLDER%¥exp2D¥",
+                                                    .File = "exp_*.txt",
+                                                    .RegexSearchFlag = True,
+                                                    .RegexPattern = "正規表現",
+                                                    .ErrorOnFoundFlag = True,
+                                                    .OutputFlag = True,
+                                                    .LabelRegexes = {New Target.LabelRegex With {
+                                                        .Folder = "%WORKFOLDER%¥exp2D¥",
+                                                        .File = "exp_*.txt",
+                                                        .LabelRegexPatten = "ITEMID",
+                                                        .LabelName = "ItemID"
+                                                        },
+                                                        New Target.LabelRegex With {
+                                                        .Folder = "%WORKFOLDER%¥exp2D¥",
+                                                        .File = "exp_*.txt",
+                                                        .LabelRegexPatten = "FILENAME",
+                                                        .LabelName = "@DB/{{@ItemID}}/001.*\n.*\n.*Clone_Name: ""(.*?)"""
+                                                        }
+                                                    },
+                                                    .OutputMessage = "{{@ITEMID}}が出力できない。{{@FILENAME}}"
+                                             }
+                                    }
+
+        '                       New Propaty With {.id = 2, .name = "田中", .sikaku = {"基本"}}}
+
+        Dim Json As String = JsonConvert.SerializeObject(Targets)
+        IO.File.WriteAllText("propatys.json", Json)
+    End Sub
+
+
+    ' JSONファイルの読み込み（配列データ）
+    Sub read()
+        Dim Json As String = IO.File.ReadAllText("propatys.json")
+        Dim Targets As Target() = JsonConvert.DeserializeObject(Of Target())(Json)
+
+        For Each Target In Targets
+            Console.WriteLine()
+            Console.WriteLine(Target.File)
+            For Each LabelRegex In Target.LabelRegexes
+                Console.WriteLine(LabelRegex.LabelName)
+            Next
+        Next
+    End Sub
+
+End Module
---
+++ dcpf_regexcheck/dcpf_regexcheck/dcpf_regexcheck/dcpflog.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Public Class dcpflog
+
+    Public pLogfile As String
+    'Public Encoding As System.Text.Encoding = System.Text.Encoding.GetEncoding("Shift_JIS")
+    Public Encoding As System.Text.Encoding = System.Text.Encoding.GetEncoding("UTF-8")
+
+    Public Sub New(ByVal logfile As String)
+        pLogfile = logfile
+
+    End Sub
+
+    Public Sub SetEncodingString(ByVal enc As String)
+        Encoding = System.Text.Encoding.GetEncoding(enc)
+    End Sub
+    Public Sub PutLog(ByVal Message As String, <CallerMemberName> Optional memberName As String = "")
+
+        Try
+            For i = 0 To 300
+                File.AppendAllText(pLogfile, Format(Now, "yyyy/MM/dd HH:mm:ss.fff") & "      " & memberName & "() " & Message & vbCrLf, Encoding)
+                Return
+            Next
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+End Class
+
